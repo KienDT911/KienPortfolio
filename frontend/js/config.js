08e3aed6c9c6942eee94a5205d793279e7243c6e
KienDT911 <kientrungbon@gmail.com>
2025-12-24 16:35:25 +0700

    Correct the domain

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,4 @@
 // Deployment config: override API base URL at runtime
 // Example: window.KB_API_BASE_URL = 'https://api.yourdomain.com';
 // Leave unset to default to http://localhost:5000
-window.KB_API_BASE_URL = window.KB_API_BASE_URL || undefined;
+window.KB_API_BASE_URL = 'https://dangtrungkien.online' || undefined;
